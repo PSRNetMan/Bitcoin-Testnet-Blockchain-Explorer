d0d6da9df23f58c6bdedf268f33f692135eef9b9
Bitcoin Tester Admin <tester@bitcointestnetserver.carolina.rr.com>
2021-11-21 11:28:13 +0000

    Gunicorn Running; Nginx Connected

--- project/settings.py
+++ project/settings.py
@@ -28,7 +28,7 @@ except KeyError as e:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.1.128"]
 
 
 # Application definition
